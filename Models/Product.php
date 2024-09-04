8620f3a574cfeb999cd933de7eddf0b156d0bbe9
battistaennio <ennio.battista99@gmail.com>
2024-09-04 17:00:41 +0200

    fix: change propriety name from name to title

--- Models/Product.php
+++ Models/Product.php
@@ -1,12 +1,12 @@
 <?php
 
 class Product {
-    public $name;
+    public $title;
     public $price;
     public $img;
 
-    public function __construct(string $_name, float $_price, string $_img){
-        $this->name = $_name;
+    public function __construct(string $_title, float $_price, string $_img){
+        $this->title = $_title;
         $this->price = $_price;
         $this->img = $_img;
     }
