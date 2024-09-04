82fd5c4779e18cba8cc00cca1d8e2d63c770e826
battistaennio <ennio.battista99@gmail.com>
2024-09-04 17:23:08 +0200

    feat: add db structure, add first product

--- data/db.php
+++ data/db.php
@@ -1,2 +1,17 @@
 <?php
 
+require_once __DIR__ . '/../Models/Product.php';
+require_once __DIR__ . '/../Models/Category.php';
+require_once __DIR__ . '/../Models/Food.php';
+
+$products = [
+
+    new Food(
+            "Crocchette Royal Canin"
+            , 58.99, 
+            "https://arcaplanet.vtexassets.com/arquivos/ids/288162/_0005_D-DERMA-HYPO-PACKSHOT-B1.jpg?v=638241437800970000",
+            "Cibo Secco",
+            [0, 1],
+            10
+    ),
+];
