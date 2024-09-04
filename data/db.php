2c79bab34e914fd3a9aa8728ff0eb2645d4d8b49
battistaennio <ennio.battista99@gmail.com>
2024-09-04 18:40:41 +0200

    feat: add more Articles

--- data/db.php
+++ data/db.php
@@ -28,14 +28,43 @@ $products = [
     ),
 
     new Article(
-        "Monge Fresh Dog Lattina", 
-        1.99, 
-        "https://arcaplanet.vtexassets.com/arquivos/ids/226830/monge-fresh-cane-adult-maiale.jpg?v=637466528133900000",
-        "Cibo Umido",
+        "Piuma con Silvervine", 
+        4.05, 
+        "https://arcaplanet.vtexassets.com/arquivos/ids/271992/croci-euphoria-bacchetta-silvervine.jpg?v=637896678260630000",
+        "Gioco",
         [1, "tutte le età"],
-        0.8,
+        0.2,
+        new Category("Gatto", '<i class="fa-solid fa-cat"></i>')
+    ),
+
+    new Article(
+        "Palla Gomma Squeaker", 
+        4.14, 
+        "https://arcaplanet.vtexassets.com/arquivos/ids/205592/Camon-palla-cane-8-cm-in-gomma-con-squeaker.jpg?v=637413929975030000",
+        "Gioco",
+        [1, "tutte le età"],
+        0.5,
         new Category("Cane", '<i class="fa-solid fa-dog"></i>')
     ),
 
+    new Article(
+        "Materasso Royal Blu", 
+        65.99, 
+        "https://arcaplanet.vtexassets.com/arquivos/ids/276554/luna-e-teo-cuccia-royal-blu.jpg?v=638034254184470000",
+        "Cuccia",
+        [0, "tutte le età"],
+        0.5,
+        new Category("Cane", '<i class="fa-solid fa-dog"></i>')
+    ),
+
+    new Article(
+        "Letto Teddy", 
+        14.39, 
+        "https://arcaplanet.vtexassets.com/arquivos/ids/278613/Teddy-grigio.jpg?v=638088607889100000",
+        "Cuccia",
+        [0, "tutte le età"],
+        0.3,
+        new Category("Gatto", '<i class="fa-solid fa-cat"></i>')
+    ),
 
 ];
